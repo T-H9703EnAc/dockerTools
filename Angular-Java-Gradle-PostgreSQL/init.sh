c334b5330ad22792961ddeef8273a49a33f0731a
T-H9703EnAc <105440150+T-H9703EnAc@users.noreply.github.com>
2022-09-04 14:12:55 +0900

    Update init.sh

--- Angular-Java-Gradle-PostgreSQL/init.sh
+++ Angular-Java-Gradle-PostgreSQL/init.sh
@@ -9,6 +9,16 @@ echo -n "任意のデータベース名を入力:"
 
 read DATABASENAME
 
+# DBのユーザ名を設定
+echo -n "データベースのユーザ名を入力:"
+
+read DBUSER
+
+# DBのパスワードを設定
+echo -n "データベースのパスワードを入力"
+
+read DBPASSWORD
+
 # 同じ名前のプロジェクトがある場合は削除
 rm -rf $PROJECTNAME
 
@@ -17,18 +27,8 @@ mkdir $PROJECTNAME
 
 
 ################ PostgresSQL関連 ###############
-
-# PostgresSQLの作業ディレクトリを作成
-mkdir $PROJECTNAME/postgres
-
-# PostgreSQL用のDockerfileを作成
-echo \
-'FROM postgres:14-alpine
-
-ENV LANG ja_JP.utf8' >> $PROJECTNAME/postgres/Dockerfile
-
 # PostgresSQLの初期データディレクトリ
-mkdir $PROJECTNAME/postgres/init
+mkdir $PROJECTNAME/db
 
 # PostgreSQLの初期データ投入SQL作成
 echo \
@@ -37,89 +37,107 @@ CREATE TABLE users(
   id integer,
   name varchar(50)
 );
-
 -- 初期データを挿入
-INSERT INTO users VALUES(1,'Tarou');" >> $PROJECTNAME/postgres/init/init.sql
+INSERT INTO users VALUES(1,'Tarou');" >> $PROJECTNAME/db/init.sql
 
 ############### Java,Gradle関連 ###############
 
 # JavaとGradleの作業ディレクトリを作成
 mkdir $PROJECTNAME/back
 
+# プロジェクトディレクトリ作成
+mkdir $PROJECTNAME/back/app
+
+# gitkeepを追加
+touch $PROJECTNAME/back/app/.gitkeep
+
 # JavaとGradle用のDockerfileを作成
 echo \
 'FROM openjdk:11
-
 RUN apt-get update
-
 RUN apt-get -y install curl
-
 RUN apt-get -y install zip
-
 RUN curl -s https://get.sdkman.io | bash\n
-
 RUN echo ". $HOME/.sdkman/bin/sdkman-init.sh; sdk install gradle" | bash
-
-VOLUME /spring_project/
-
-WORKDIR /sprin_project/' >> $PROJECTNAME/back/Dockerfile
+WORKDIR /app/' >> $PROJECTNAME/back/Dockerfile
 
 ############### Angular,Node.js関連 ###############
 # Angular用の作業ディレクトリを作成
 mkdir $PROJECTNAME/front
 
+# プロジェクトディレクトリ作成
+mkdir $PROJECTNAME/front/app
+
+# gitkeep作成
+touch $PROJECTNAME/front/app/.gitkeep
+
 # AngularとNode.js用のDockerfileを作成
 echo \
 'FROM node:latest
-
 WORKDIR /app
-
 RUN npm install -g npm && \
     npm install -g @angular/cli
-
 EXPOSE 4200' >> $PROJECTNAME/front/Dockerfile
 
 ############### docker-compose.yml ###############
 echo \
 "version: '3.8'
 services:
-  postgres:
-    image: postgres:14-alpine
-    container_name: postgres
-    build: ./postgres
+  db:
+    image: postgres:12.11
+    container_name: db
     restart: always
     environment:
-      POSTGRES_USER: user
-      POSTGRES_PASSWORD: password
+      POSTGRES_USER: $DBUSER
+      POSTGRES_PASSWORD: $DBPASSWORD
       POSTGRES_DB: $DATABASENAME
       TZ: "Asia/Tokyo"
     ports:
       - 5432:5432
     volumes:
-      - ./postgres/init:/docker-entrypoint-initdb.d
+      - ./db:/docker-entrypoint-initdb.d
   back:
     container_name: back
     build: ./back
     depends_on:
-      - postgres
+      - db
     ports:
-      - "8080:8080"
+      - 8080:8080
     tty: true
     volumes:
-      - ./back/spring_project:/spring_project:cached
-    working_dir: /spring_project
+      - ./back/app:/app:cached
+    working_dir: /app
   front:
     container_name: front
     build: ./front
     ports:
-      - "4200:4200"
+      - 4200:4200
     volumes:
-      - ./front/angular_project:/app:cached
+      - ./front/project:/app:cached
     stdin_open: true
     tty: true
 volumes:
   postgres:
     driver: local" >> $PROJECTNAME/docker-compose.yml
+
+############### .devcontainer作成 ###############
+mkdir $PROJECTNAME/.devcontainer
+
+echo \
+"{
+    \"name\": \"back\",
+    \"workspaceFolder\": \"/app\",
+    \"dockerComposeFile\": \"../docker-compose.yml\",
+    \"settings\": {
+        \"terminal.integrated.defaultProfile.linux\": \"/bin/bash\",
+    },
+    \"service\": \"back\", //attachするコンテナはbackを指定
+    \"extensions\": [
+        \"vscjava.vscode-java-pack\", // JavaExtensionPack
+        \"pivotal.vscode-boot-dev-pack\", // Spring Boot Extension Pack
+        \"gabrielbb.vscode-lombok\" //Lombok Annotations Support For VS Code
+    ]
+}" >> $PROJECTNAME/.devcontainer/devcontainer.json
 
 ############### Docker コンテナ作成・起動・削除 シェル作成 ###############
 # コンテナ作成・起動シェル作成
@@ -137,16 +155,14 @@ sudo docker system prune -a' >> $PROJECTNAME/deleteDocker.sh
 
 # コンテナに入るシェルの作成
 echo \
-"echo 'postgres --> 1 /back --> 2 / front --> 3'
+"echo 'db --> 1 /back --> 2 / front --> 3'
 echo -n "開きたいコンテナを選択してください:"
-
 read SELECTCONTAINER
-
 if [ \$SELECTCONTAINER -eq 1 ]; then
     echo [postgres]コンテナを開きます。
     echo PostgresSQLへの接続コマンドは以下のコマンドを実行してください
-    echo psql -U user -d $DATABASENAME
-    sudo docker exec -it postgres /bin/bash
+    echo psql -U $DBUSER -d $DATABASENAME
+    sudo docker exec -it db /bin/bash
 elif [ \$SELECTCONTAINER -eq 2 ]; then
     echo [back]コンテナを開きます。
     echo JavaとGradleのVersion確認コマンドは以下を実行してください
